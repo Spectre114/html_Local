23fa8d3ca118aa200be4c7b3ffc6b1254bc53fed
Kartickey Aggarwal <hmtv2003@gmail.com>
2024-05-09 11:14:08 +0530

    Latest Commit:

--- htdocs/AJAX/getData.php
+++ htdocs/AJAX/getData.php
@@ -5,17 +5,25 @@ $password = "";
 $db = "db";
 $conn = mysqli_connect($server, $username, $password, $db);
 $roll = $_REQUEST['roll'];
-$sql = "SELECT * FROM student WHERE rollno = $roll";
-$res = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($res) > 0) {
-    while ($row = mysqli_fetch_assoc($res)) {
-        echo "Roll No- " . $row['rollno'];
-        echo "<br>";
-        echo " Name- " . $row['name'];
-        echo "<br>";
-        echo "<br>";
+if ($roll == '')
+    echo 'Data Not Found';
+else {
+    $sql = "SELECT * FROM student WHERE rollno = $roll";
+    $res = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($res) > 0) {
+        $found = false;
+        while ($row = mysqli_fetch_assoc($res)) {
+            $found = true;
+            echo "Roll No- " . $row['rollno'];
+            echo "<br>";
+            echo " Name- " . $row['name'];
+            echo "<br>";
+            echo "<br>";
+        }
+        if (!$found) {
+            echo "No data found for Roll No: " . $roll;
+        }
+    } else {
+        echo "Underflow";
     }
-} else {
-    echo "Underflow";
 }
