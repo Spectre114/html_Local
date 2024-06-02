babca1d44e7d5e4b5a41cef0568efc6fd57ef53e
Spectre114 <hmtv2003@gmail.com>
2024-06-02 14:18:47 +0530

    Login code

---
+++ htdocs/LoginHome/save.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "db";
+$conn = mysqli_connect($server, $username, $password, $database);
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+$sql = "SELECT * FROM user WHERE username = '$email' AND password = '$pass'";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $_SESSION['id'] = $row['id'];
+    $_SESSION['name'] = $row['username'];
+    header("location: homePage.php");
+} else {
+    header("location: login_err.html");
+}
